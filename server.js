cec1de0a48c19b93c379fc055bb9da15ad01cb34
Janne Kemi <janne@kemikal.org>
2026-09-25 10:47:09 +0200

    Värd skapar spel och får rumskod. Closes #1

--- server.js
+++ server.js
@@ -18,10 +18,31 @@ app.get("/health", (_req, res) =>
 // Alla spel lever i minnet: rumskod -> spel
 const games = new Map();
 
+function newCode() {
+  let code;
+  do code = String(Math.floor(1000 + Math.random() * 9000));
+  while (games.has(code));
+  return code;
+}
+
+function lobby(game) {
+  return { code: game.code, players: [...game.players.values()].map((p) => p.name) };
+}
+
 io.on("connection", (socket) => {
   console.log("connect", socket.id);
 
-  // TODO (live): host:create, player:join, host:start, player:answer, host:next
+  socket.on("host:create", (ack) => {
+    const code = newCode();
+    const game = { code, host: socket.id, players: new Map(), current: -1, answers: new Map() };
+    games.set(code, game);
+    socket.join(code);
+    socket.data.code = code;
+    socket.data.role = "host";
+    console.log("spel skapat", code);
+    ack({ code });
+    io.to(code).emit("game:lobby", lobby(game));
+  });
 
   socket.on("disconnect", (reason) => console.log("disconnect", socket.id, reason));
 });
